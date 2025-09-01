3fa1df8bb69496357c6a638a8c5c250e4a3988e3
Diana <user@users-MacBook-Pro.local>
2025-09-01 11:03:23 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+
+import { ref } from 'vue'
+
+
+const header = ref('Shopping list app')
+const items = ref([
+  {id:1, label:'Milk'},
+  {id:2, label: 'Bread'},
+  {id:3, label: 'Eggs'}
+  ])
+</script>
+
+<template>
+  <h1>{{header}}</h1>
+  <ul> 
+    <li v-for="({id, label}, index) in items" :key="id">{{index}}. {{label}}</li>
+ </ul>
+ 
+</template>
+
+<style scoped>
+h1{
+  color: #42b983;
+  font-weight: normal;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
